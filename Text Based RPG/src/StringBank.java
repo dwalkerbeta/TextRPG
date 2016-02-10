58304ec83d447d6f564b361ba2296ab01af25bd1
dbeta <dwalker.beta@gmail.com>
2016-02-10 12:37:19 -0500

    Second Commit
    was #2 with  \n chars added at the ends of things

--- Text Based RPG/src/StringBank.java
+++ Text Based RPG/src/StringBank.java
@@ -2,28 +2,28 @@
 public class StringBank {
 
 	// This is a list of all prompts for the beginning of the game
-	public static String askcharname = "What is your name?";
+	public static String askcharname = "What is your name?\n";
 
-	public static String askcharclass = "What is your class? (Choose a number) \n1. Warrior\n2. Rogue\n3. Wizard";
+	public static String askcharclass = "What is your class? (Choose a number) \n1. Warrior\n2. Rogue\n3. Wizard\n";
 
 	//re-used strings
-	public static String entercommand = "Please enter a command";
+	public static String entercommand = "Please enter a command\n";
 
-	public static String error = "Sorry, that is not a valid input";
+	public static String error = "Sorry, that is not a valid input\n";
 	
 	// start of main strings
 	
 	// choice = 0
-	public static String str1 = "You find yourself in the clearing of a forest. There is a weapon on the ground, do you take it? (y/n)";
+	public static String str1 = "You find yourself in the clearing of a forest. There is a weapon on the ground, do you take it? (y/n)\n";
 	
-	public static String str1y = "You take the weapon.";
-	public static String str1n = "You leave the weapon behind. Good luck.";
+	public static String str1y = "You take the weapon.\n";
+	public static String str1n = "You leave the weapon behind. Good luck.\n";
 	
 	// choice = 1	
-	public static String str1_1 = "Having taken your weapon you set off exploring. After some time you come across a fork in the road. The right path looks sunny and the left path leads further into the forest. Which path do you follow? (right/left)";
+	public static String str1_1 = "Having taken your weapon you set off exploring. After some time you come across a fork in the road. The right path looks sunny and the left path leads further into the forest. Which path do you follow? (right/left)\n";
 
-	public static String str1_1right = "";
-	public static String str1_1left = "";
+	public static String str1_1right = "Right\n";
+	public static String str1_1left = "Left\n";
 	
 	
 }
